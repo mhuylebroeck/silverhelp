a2b19667b0fdce320041815f3876d5d17e06d438
Marti Giro <martigirocorcoll@gmail.com>
2021-11-23 18:07:25 +0100

    keep work on form

--- app/controllers/help_requests_controller.rb
+++ app/controllers/help_requests_controller.rb
@@ -6,17 +6,17 @@ class HelpRequestsController < ApplicationController
 
   def create
     @help_request = HelpRequest.new(helper_params)
-    @help_request.senior_id = current_user
-    if @help_request.save
+    @help_request.senior_id = current_user.id
+    if @help_request.save!
       redirect_to users_path
     else
       render "help_requests/new"
-  end
+    end
   end
 
   private
 
   def helper_params
-    params.require(:help_request).permit(:start_time, :duration, :location, :task)
+    params.require(:help_request).permit(:start_time, :duration, :location, :task_id)
   end
 end
