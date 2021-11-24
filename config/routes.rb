e91d85bcee4c845b1d81d62cf47b15c7f68b8fa6
lucypinker <lucypinker22@gmail.com>
2021-11-24 16:52:00 +0100

    added edit to help requests resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
-  resources :help_requests, only: [:new, :create]
+  resources :help_requests, only: [:new, :create, :edit]
   resource :dashboard, only: [:show]
 end
