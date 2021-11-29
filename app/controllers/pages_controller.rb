ce5e59a9a9f7ce354705ec6aeb1970cb0093baa5
Marti Giro <martigirocorcoll@gmail.com>
2021-11-29 12:18:08 +0100

    multi form made

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @help_request = HelpRequest.new
+    @tasks = Task.all
   end
 end
