5826eb8ae2796e8ed492b3654a3901934cd8bc64
Marti Giro <martigirocorcoll@gmail.com>
2021-11-30 09:34:52 +0100

    migration problem solved

--- db/migrate/20211129141729_add_references_to_favorite_helper.rb
+++ db/migrate/20211129141729_add_references_to_favorite_helper.rb
@@ -1,6 +1,6 @@
 class AddReferencesToFavoriteHelper < ActiveRecord::Migration[6.0]
   def change
-    add_reference :favorites, :senior, foreign_key: { to_table: :users }
-    add_reference :favorites, :helper, foreign_key: { to_table: :users }
+    add_reference :favorite_helpers, :senior, foreign_key: { to_table: :users }
+    add_reference :favorite_helpers, :helper, foreign_key: { to_table: :users }
   end
 end
