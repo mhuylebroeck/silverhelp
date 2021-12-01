56494899e5b3cf764b3cbd8566295f011476aa3c
Marti Giro <martigirocorcoll@gmail.com>
2021-12-01 12:00:15 +0100

    added functional previous step btn

--- app/javascript/controllers/multi_step_form_controller.js
+++ app/javascript/controllers/multi_step_form_controller.js
@@ -14,4 +14,15 @@ export default class extends Controller {
 
     // coger al elemento en el index - 1: agregarle el d-none
   }
+
+  goToPrevious(event) {
+    event.preventDefault();
+    const previous = this.stepTargets[event.currentTarget.dataset.nextStep];
+    const current = this.stepTargets[event.currentTarget.dataset.nextStep - 1];
+    previous.classList.add("d-none");
+    current.classList.remove("d-none");
+    // coger al elemento en el index next-step: quitarle el d-none
+
+    // coger al elemento en el index - 1: agregarle el d-none
+  }
 }
